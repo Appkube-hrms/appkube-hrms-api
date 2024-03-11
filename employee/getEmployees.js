c879b805e1d5550668287759d1775790feb31b9d
SohailAli20 <mohammad.ali@synectiks.com>
2024-03-11 17:21:46 +0530

    minor change

--- employee/getEmployees.js
+++ employee/getEmployees.js
@@ -2,7 +2,8 @@ const { connectToDatabase } = require("../db/dbConnector");
 const middy = require("middy");
 const { errorHandler } = require("../util/errorHandler");
 
-exports.handler = middy(async (event) => {
+exports.handler = middy(async (event, context) => {
+    context.callbackWaitsForEmptyEventLoop = false;
     let page = event.queryStringParameters?.page ?? null;
     if (page == null) {
         page = 1;
@@ -12,8 +13,6 @@ exports.handler = middy(async (event) => {
     let offset = (page - 1) * 10;
     offset = Math.max(offset, 0);
     const client = await connectToDatabase();
-    console.info("connected to database :", JSON.stringify(client))
-    console.log("offset :",offset);
     const totalPagesQuery = `
                 SELECT COUNT(*) AS total_count
                 FROM employee e
@@ -43,11 +42,9 @@ exports.handler = middy(async (event) => {
                 LIMIT 10 OFFSET ${offset}
     `;
     const totalPagesResult = await client.query(totalPagesQuery);
-    console.info("query result 1:", JSON.stringify(totalPagesResult.rows))
     const totalRecords = totalPagesResult.rows[0].total_count;
     const totalPages = Math.ceil(totalRecords / limit);
     const employeeMetaData = await client.query(query);
-    console.info("query result 2:", JSON.stringify(employeeMetaData.rows))
     const resultArray = employeeMetaData.rows.map((row) => ({
         employee_name: `${row.first_name} ${row.last_name}`,
         id: row.id,
@@ -57,7 +54,6 @@ exports.handler = middy(async (event) => {
         department: row.department,
         start_date: row.start_date,
     }));
-    console.info("result array", JSON.stringify(resultArray))
     return {
         statusCode: 200,
         headers: {
