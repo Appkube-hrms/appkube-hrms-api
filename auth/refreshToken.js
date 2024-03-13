561fbbc9c4a5bc6a8bf7b95c66fafb4581f0df3a
Chetan Meegada <chetan.meegada@synectiks.com>
2024-03-13 14:36:18 +0530

    minor changes

--- auth/refreshToken.js
+++ auth/refreshToken.js
@@ -1,15 +1,14 @@
-const AWS = require('aws-sdk');
+const { CognitoIdentityProviderClient, InitiateAuthCommand } = require("@aws-sdk/client-cognito-identity-provider");
+require("dotenv").config();
 const crypto = require('crypto');
 const { connectToDatabase } = require("../db/dbConnector");
-const cognitoIdentityProvider = new AWS.CognitoIdentityServiceProvider();
-
 exports.handler = async (event) => {
     const requestBody = JSON.parse(event.body);
-    const {email} = requestBody;
-     const authHeader =
-		event.headers.Authorization || event.headers.authorization;
-        const token = authHeader.substring(7); 
-        const client = await connectToDatabase();
+    const { email } = requestBody;
+    const authHeader = event.headers.Authorization || event.headers.authorization;
+    const token = authHeader.substring(7); 
+    const client = await connectToDatabase();
+
     try {
         const params = {
             ClientId: process.env.COGNITO_CLIENT_ID,
@@ -19,22 +18,25 @@ exports.handler = async (event) => {
                 'SECRET_HASH': getSecretHash(email)
             }
         };
-        const data = await cognitoIdentityProvider.initiateAuth(params).promise();
+        const client = await connectToDatabase();
+        const client1 = new CognitoIdentityProviderClient({ region: "us-east-1" });
+        const command = new InitiateAuthCommand(params);
+        const data = await client1.send(command);
+
         await client.query(
-            `
-                    UPDATE employee
-                    SET access_token = $1
-                    WHERE
-                        work_email = $2`,
-            [data.AuthenticationResult.AccessToken,email]
+            `UPDATE employee
+             SET access_token = $1
+             WHERE work_email = $2`,
+            [data.AuthenticationResult.AccessToken, email]
         );
+
         return {
-			statusCode: 200,
-			headers: {
-				"Access-Control-Allow-Origin": "*",
-			},
-			body: JSON.stringify({Access_token : data.AuthenticationResult.AccessToken}),
-		};
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+            },
+            body: JSON.stringify({ Access_token : data.AuthenticationResult.AccessToken }),
+        };
     } catch (error) {
         return error.message;
     }
@@ -42,7 +44,6 @@ exports.handler = async (event) => {
 
 function getSecretHash(email) {
     const clientId = process.env.COGNITO_CLIENT_ID;
-    const date = new Date().toISOString().substring(0, 10);
     const message = email + clientId;
     const secretHash = crypto.createHmac('SHA256', clientId).update(message).digest('base64');
     return secretHash;
