cf41dcd4eda1b3dc26683fb8656654840720fed7
SohailAli20 <mohammad.ali@synectiks.com>
2024-04-05 16:53:22 +0530

    api fix

--- employee/getEmployee.js
+++ employee/getEmployee.js
@@ -11,53 +11,64 @@ const idSchema = z.object({
 
 exports.handler = middy(async (event, context) => {
 	context.callbackWaitsForEmptyEventLoop = false
-	const employeeId = event.pathParameters.id 
-	console.log("id : ", employeeId);
+	const employeeId = event.pathParameters.id
 	const client = await connectToDatabase()
 
 	const query = `
-    SELECT
-        o.*,
-        e.*,
-        a.*,
-        ed.*,
-        d.name AS department_name,
-        et.type AS emp_type,
-        edg.id AS emp_designation_id,
-        edg.designation AS emp_designation,
-        m.first_name AS reporting_manager_first_name,
-        m.last_name AS reporting_manager_last_name,
-        doc.id AS document_id,
-        doc.*,
-        eq.*,
-        dt.name AS device_type_name
-    FROM
-        employee e
-    LEFT JOIN
-        address a ON e.id = a.emp_id
-    LEFT JOIN
-        emp_detail ed ON e.id = ed.emp_id
-    LEFT JOIN
-        department d ON ed.department_id = d.id
-    LEFT JOIN
-        emp_type et ON ed.emp_type_id = et.id
-    LEFT JOIN
-        emp_designation edg ON ed.designation_id = edg.id
-    LEFT JOIN
-        employee m ON ed.reporting_manager_id = m.id
-    LEFT JOIN
-        document doc ON doc.emp_id = e.id
-    LEFT JOIN
-        equipment eq ON eq.emp_id = e.id
-    LEFT JOIN
-        device_type dt ON eq.device_type_id = dt.id
-    LEFT JOIN
-        organisation o ON e.org_id = o.id  
-    WHERE
-        e.id = $1
-`
+	SELECT
+    o.id AS org_id,
+    o.*, 
+    e.id AS emp_id,
+    e.*, 
+    a.id AS address_id,
+    a.*, 
+    ed.id AS emp_detail_id,
+    ed.*, 
+    d.name AS department_name,
+    et.type AS emp_type,
+    edg.id AS emp_designation_id,
+    edg.designation AS emp_designation,
+    m.first_name AS reporting_manager_first_name,
+    m.last_name AS reporting_manager_last_name,
+    jsonb_agg(doc.*) AS documents,
+    (
+        SELECT
+            jsonb_agg(jsonb_build_object('id', eq.id,'owner', eq.owner,'device_type_name', dt.name,
+                'serial_number', eq.serial_number,
+                'note', eq.note,
+                'supply_date', eq.supply_date, 'device_type_name', dt.name))
+        FROM
+            equipment eq
+        LEFT JOIN
+            device_type dt ON eq.device_type_id = dt.id
+        WHERE
+            eq.emp_id = e.id
+    ) AS equipments-- Aggregate equipment details along with device type name
+FROM
+    employee e
+LEFT JOIN
+    address a ON e.id = a.emp_id
+LEFT JOIN
+    emp_detail ed ON e.id = ed.emp_id
+LEFT JOIN
+    department d ON ed.department_id = d.id
+LEFT JOIN
+    emp_type et ON ed.emp_type_id = et.id
+LEFT JOIN
+    emp_designation edg ON ed.designation_id = edg.id
+LEFT JOIN
+    employee m ON ed.reporting_manager_id = m.id
+LEFT JOIN
+    document doc ON doc.emp_id = e.id
+LEFT JOIN
+    organisation o ON e.org_id = o.id  
+WHERE
+    e.id = $1::uuid
+GROUP BY
+    o.id, e.id, a.id, ed.id, d.name, et.type, edg.id, m.first_name, m.last_name`
 
 	const result = await client.query(query, [employeeId])
+	console.log(JSON.stringify(result.rows[0]))
 	await client.end()
 
 	const formattedResult = {
@@ -67,89 +78,60 @@ exports.handler = middy(async (event, context) => {
 		documents: {},
 		equipment: [],
 	}
+	const data = result.rows[0]
+	formattedResult.personal_information = {
+		id: data.emp_id,
+		email: data.email,
+		work_email: data.work_email,
+		image: data.image,
+		first_name: data.first_name,
+		last_name: data.last_name,
+		gender: data.gender,
+		dob: data.dob,
+		number: data.number,
+		emergency_number: data.emergency_number,
+		highest_qualification: data.highest_qualification,
+		address_id: data.address_id,
+		address_line_1: data.address_line_1,
+		address_line_2: data.address_line_2,
+		landmark: data.landmark,
+		country: data.country,
+		state: data.state,
+		city: data.city,
+		zipcode: data.zipcode,
+	}
+	formattedResult.organization_details = {
+		org_id: data.org_id,
+		org_name: data.name,
+		org_logo: data.logo || "",
+		org_address_line_1: data.address_line_1,
+		org_address_line_2: data.address_line_2,
+		org_contact: data.number,
+		org_country: data.country,
+		org_city: data.city,
+		org_state: data.state,
+		org_Zipcode: data.zipcode,
+	}
+	formattedResult.professional_information = {
+		emp_detail_id: data.emp_detail_id,
+		designation_id: data.emp_designation_id,
+		pf: data.pf,
+		uan: data.uan,
+		department_id: data.department_id,
+		reporting_manager_id: data.reporting_manager_id,
+		emp_type_id: data.emp_type_id,
+		work_location: data.work_location,
+		start_date: data.start_date,
+		department_name: data.department_name,
+		emp_type: data.emp_type,
+		emp_designation_id: data.emp_designation_id,
+		emp_designation: data.emp_designation,
+		reporting_manager_first_name: data.reporting_manager_first_name || "",
+		reporting_manager_last_name: data.reporting_manager_last_name || "",
+	}
 
-	result.rows.forEach(row => {
-		if (!formattedResult.personal_information.emp_id) {
-			formattedResult.personal_information = {
-				emp_id: row.emp_id,
-				email: row.email,
-				image: row.image || "",
-				password: row.password,
-				work_email: row.work_email,
-				first_name: row.first_name,
-				last_name: row.last_name,
-				gender: row.gender,
-				dob: row.dob,
-				number: row.number,
-				emergency_number: row.emergency_number,
-				highest_qualification: row.highest_qualification,
-				address_id: row.address_id,
-				address_line_1: row.address_line_1,
-				address_line_2: row.address_line_2,
-				landmark: row.landmark,
-				country: row.country,
-				state: row.state,
-				city: row.city,
-				zipcode: row.zipcode,
-			}
-		}
-		if (!formattedResult.organization_details.org_id) {
-			formattedResult.organization_details = {
-				org_id: row.org_id,
-				org_name: row.name,
-				org_logo: row.logo || "",
-				org_address_line_1: row.address_line_1,
-				org_address_line_2: row.address_line_2,
-				org_contact: row.number,
-				org_country: row.country,
-				org_city: row.city,
-				org_state: row.state,
-				org_Zipcode: row.zipcode,
-			}
-		}
-
-		if (!formattedResult.professional_information.emp_detail_id) {
-			formattedResult.professional_information = {
-				emp_detail_id: row.emp_detail_id,
-				designation_id: row.designation_id,
-				pf: row.pf,
-				uan: row.uan,
-				department_id: row.department_id,
-				reporting_manager_id: row.reporting_manager_id,
-				emp_type_id: row.emp_type_id,
-				work_location: row.work_location,
-				start_date: row.start_date,
-				department_name: row.department_name,
-				emp_type: row.emp_type,
-				emp_designation_id: row.emp_designation_id,
-				emp_designation: row.emp_designation,
-				reporting_manager_first_name:
-					row.reporting_manager_first_name || "",
-				reporting_manager_last_name:
-					row.reporting_manager_last_name || "",
-			}
-		}
-
-		if (row.document_id) {
-			formattedResult.documents[row.document_id] = {
-				document_id: row.document_id,
-				name: row.name,
-				url: row.url,
-			}
-		}
-
-		if (row.device_type_name) {
-			formattedResult.equipment.push({
-				owner: row.owner,
-				device_type_id: row.device_type_id,
-				manufacturer: row.manufacturer,
-				serial_number: row.serial_number,
-				note: row.note,
-				supply_date: row.supply_date,
-				device_type_name: row.device_type_name,
-			})
-		}
-	})
+	formattedResult.documents = data.documents
+	formattedResult.equipment = data.equipments 
 
 	formattedResult.documents = Object.values(formattedResult.documents)
 	return {
@@ -163,7 +145,3 @@ exports.handler = middy(async (event, context) => {
 	.use(authorize())
 	.use(pathParamsValidator(idSchema))
 	.use(errorHandler())
-
-function formatResult(rows) {
-	return formattedResult
-}
